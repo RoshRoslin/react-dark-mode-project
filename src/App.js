3678e8c77ad182b19591c6014aa5b712a3a66a07
RoshRoslin <RoshRoslin@gmail.com>
2021-05-17 11:39:13 -0700

    source code, first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import data from './data';
+import Article from './Article';
+
+const getStorageTheme = () => {
+  let theme = 'light-theme';
+
+  if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme');
+  }
+
+  return theme;
+};
+
+function App() {
+  const [theme, setTheme] = useState(getStorageTheme);
+
+  const handleTheme = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme');
+    } else {
+      setTheme('light-theme');
+    }
+  };
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  return (
+    <main>
+      <div className='nav-center'>
+        <h1>Overreacted</h1>
+        <button className='btn' onClick={handleTheme}>
+          Toggle Theme
+        </button>
+      </div>
+      <section className='articles'>
+        {data.map((item) => {
+          return <Article key={item.id} {...item}></Article>;
+        })}
+      </section>
+    </main>
+  );
+}
+
+export default App;
